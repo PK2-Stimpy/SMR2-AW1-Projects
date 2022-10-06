02940c695890d4888afc9620d5fbd4de5e244f6a
PK2_Stimpy <31771533+PK2-Stimpy@users.noreply.github.com>
2022-10-06 20:00:53 +0200

    tt

--- TESTING/P1/include/script.js
+++ TESTING/P1/include/script.js
@@ -1,3 +1,15 @@
+let animales = 0;
+
+function add() {
+    animales++;
+    alert("Has añadido un animal domestico, ahora hay " + animales);
+}
+
 function comprar() {
-    alert("Nos hemos quedado sin animales domesticos.");
+    if(animales <= 0)
+        alert("Nos hemos quedado sin animales domesticos.");
+    else {
+        animales--;
+        alert("Has comprado un animal domestico, ahora hay " + animales);
+    }
 }
