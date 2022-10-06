3fae2dc558b35dc97e778f7836fbd6d54f553b3a
PK2_Stimpy <31771533+PK2-Stimpy@users.noreply.github.com>
2022-10-06 20:25:02 +0200

    s

--- TESTING/P2/index.php
+++ TESTING/P2/index.php
@@ -2,5 +2,19 @@
 $ip = $_SERVER['REMOTE_ADDR'];
 $name = $_GET['name'];
 
-echo("Hola! Tu nombre es: $name y tu ip es: $ip");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sedeño busca esposo</title>
+</head>
+<body>
+    <center>
+        <h1>SEDEÑO BUSCA ESPOSO</h1>
+        <h3>El esposo que busca se llama: <?php echo($name); ?>. Y su ip es: <?php echo($ip); ?></h3>
+        <h4>ESPERA! Eres tu? Sedeño te está buscando! Corre y dale un morreo!</h4>
+    </center>
+</body>
+</html>
