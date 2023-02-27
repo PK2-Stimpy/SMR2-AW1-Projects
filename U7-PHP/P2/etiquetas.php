c4145ff2a83668f1a69fbd49b77fa73e1e3c4b74
PK2_Stimpy <thefernan1@gmail.com>
2023-02-27 17:14:11 +0100

    etiquetas.php

---
+++ U7-PHP/P2/etiquetas.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Programa de impresión de nombres</title>
+    </head>
+
+    <body>
+        <?php
+        // Este programa permite mostrar un nombre y apellidos en diferentes colores
+        echo"<font color='red'>Fernando Javier</font> <br>";
+        echo"<font color='gray'>García Hurtado</font>";
+
+        // En este paso imprimiremos
+        // el curso y el grupo
+        echo"<b><u>2º CFGM SMR</u></b>";
+        ?>
+    </body>
+</html>
