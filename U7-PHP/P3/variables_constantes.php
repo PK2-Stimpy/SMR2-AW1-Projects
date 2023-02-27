ff33fea5c6a5259cc90eb304e2fe59fd9e4cf37e
PK2_Stimpy <thefernan1@gmail.com>
2023-02-27 17:30:37 +0100

    Vr_0

---
+++ U7-PHP/P3/variables_constantes.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Trabajando con constantes</title>
+    </head>
+
+    <body>
+        <?php
+        define("Vr_0", 0.42215773311582662702); // Volumen del Tetraedro de Reuleaux
+        define("Vc_0", 1.26408473530530111307); // Constante de Vardi
+        define("AfM_0", 1.5065918849); // Área del fractal de Mandelbrot
+        define("S1_0", 1.09317045919549089396); // Constante de Smarandache
+        define("q_0", 1.78723165018296593301); // Constante de Komornik
+
+        const Vr_1 = 0.42215773311582662702;
+        const Vc_0 = 1.26408473530530111307;
+        const AfM_0 = 1.5065918849;
+        const S1_0 = 1.09317045919549089396;
+        const q_0 = 1.78723165018296593301;
+
+        echo'Vr_0 = $Vr_0';
+        ?>
+    </body>
+</html>
