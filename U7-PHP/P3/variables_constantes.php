20f77fbd60cef08d6612ee9cea43d0643bfd98c1
PK2_Stimpy <thefernan1@gmail.com>
2023-02-27 17:37:29 +0100

    echo '<br>';

--- U7-PHP/P3/variables_constantes.php
+++ U7-PHP/P3/variables_constantes.php
@@ -12,40 +12,45 @@
         define("q_0", 1.78723165018296593301); // Constante de Komornik
 
         const Vr_1 = 0.42215773311582662702;
-        const Vc_0 = 1.26408473530530111307;
-        const AfM_0 = 1.5065918849;
-        const S1_0 = 1.09317045919549089396;
-        const q_0 = 1.78723165018296593301;
+        const Vc_1 = 1.26408473530530111307;
+        const AfM_1 = 1.5065918849;
+        const S1_1 = 1.09317045919549089396;
+        const q_1 = 1.78723165018296593301;
 
         // Vr
         echo 'Vr = ';
         echo Vr_0;
         echo ' & ';
         echo constant('Vr_1');
+        echo '<br>';
 
         // Vc
         echo 'Vc = ';
         echo Vc_0;
         echo ' & ';
         echo constant('Vc_1');
+        echo '<br>';
 
         // AfM
         echo 'AfM = ';
         echo AfM_0;
         echo ' & ';
         echo constant('AfM_1');
+        echo '<br>';
 
         // S1
         echo 'S1 = ';
         echo S1_0;
         echo ' & ';
         echo constant('S1_1');
+        echo '<br>';
 
         // q
         echo 'q = ';
         echo q_0;
         echo ' & ';
         echo constant('q_1');
+        echo '<br>';
         ?>
     </body>
 </html>
