0b0a463830979a5554b75c42a5af29725cb34d4d
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 18:22:26 +0100

    P7-4

--- U7-PHP/P7/estructuras_control_rep.php
+++ U7-PHP/P7/estructuras_control_rep.php
@@ -28,6 +28,18 @@
         for($contador = 1; $contador <= 100; $contador++)
             $suma+=$contador;
         echo("Resultado: $suma<br>");
+
+        echo"<h1>MESES</h1>";
+        $meses_esp = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+        $meses_eng = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+        echo"<h2>ESPAÑOL</h2>";
+        foreach ($meses_esp as $mes)
+            echo("$mes <br>");
+        
+        echo"<h2>INGLÉS</h2>";
+        foreach ($meses_eng as $mes)
+            echo("$mes <br>");
         ?>
     </body>
 </html>
