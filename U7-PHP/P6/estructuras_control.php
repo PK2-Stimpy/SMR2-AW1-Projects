e339fe0d631203ba113c85645b327225deaade02
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 17:34:45 +0100

    P6-2

--- U7-PHP/P6/estructuras_control.php
+++ U7-PHP/P6/estructuras_control.php
@@ -6,7 +6,7 @@
     <body>
         <?php
         $velocidad = 200;
-        $aceleracion = 40;
+        $aceleracion = 6;
         $altura = 105;
 
         const GRAVITACIONAL = 6.674e11;
@@ -19,6 +19,30 @@
             echo("La velocidad es excesiva.");
         else if($aceleracion < ACELERACION_G)
             echo("La aceleración es insuficiente.");
+
+        switch ($aceleracion) {
+            case 0:
+            case 1:
+            case 2:
+                echo("Hay que aumentar la aceleración.");
+                break;
+            case 3:
+                echo("Falta algo de aceleración.");
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                echo("La aceleración es perfecta.");
+                break;
+            case 8:
+            case 9:
+                echo("Hay que decelerar por peligro de destrucción.");
+                break;
+            default:
+                echo("La cifra no está contemplada.");
+                break;
+        }
         ?>
     </body>
 </html>
