7040ba0b9fc6d433de872db674b738124ca11a88
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 17:11:47 +0100

    &

--- U7-PHP/P5/operadores_comparacion.php
+++ U7-PHP/P5/operadores_comparacion.php
@@ -15,6 +15,10 @@
         echo("¿El valor de operaciones semanales es mayor igual que el máximo? " . ($operaciones_semanales >= operaciones_max_semanales ? 'Sí':'No') . '<br>');
         echo("¿El valor de operaciones diarias es exactamente igual al máximo? " . ($operaciones_diarias == operaciones_max_diarias ? 'Sí':'No') . '<br>');
         echo("¿El valor de operaciones diarias es distinto al de operaciones semanales? " . ($operaciones_diarias != operaciones_max_semanales ? 'Sí':'No') . '<br>');
+
+        $indicadores = 13;
+        $filtro = 12;
+        echo("El resultado de las operaciones and con los indicadores y el filtro es: " . ($indicadores & $filtro) . '<br>');
         ?>
     </body>
 </html>
