528e252e72f71e03935f163116c53411eabfd31b
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 17:09:49 +0100

    . '<br>'

--- U7-PHP/P5/operadores_comparacion.php
+++ U7-PHP/P5/operadores_comparacion.php
@@ -11,10 +11,10 @@
         const operaciones_max_diarias = 4;
         const operaciones_max_semanales = 8;
 
-        echo("¿El valor de operaciones diarias es mayor igual que el máximo? " . ($operaciones_diarias >= operaciones_max_diarias));
-        echo("¿El valor de operaciones semanales es mayor igual que el máximo? " . ($operaciones_semanales >= operaciones_max_semanales));
-        echo("¿El valor de operaciones diarias es exactamente igual al máximo? " . ($operaciones_diarias == operaciones_max_diarias));
-        echo("¿El valor de operaciones diarias es distinto al de operaciones semanales? " . ($operaciones_diarias != operaciones_semanales));
+        echo("¿El valor de operaciones diarias es mayor igual que el máximo? " . ($operaciones_diarias >= operaciones_max_diarias ? 'Sí':'No') . '<br>');
+        echo("¿El valor de operaciones semanales es mayor igual que el máximo? " . ($operaciones_semanales >= operaciones_max_semanales ? 'Sí':'No') . '<br>');
+        echo("¿El valor de operaciones diarias es exactamente igual al máximo? " . ($operaciones_diarias == operaciones_max_diarias ? 'Sí':'No') . '<br>');
+        echo("¿El valor de operaciones diarias es distinto al de operaciones semanales? " . ($operaciones_diarias != operaciones_semanales ? 'Sí':'No') . '<br>');
         ?>
     </body>
 </html>
