16e86372b49e7f83ed679e0db7d34a0f4bde3d0a
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 17:43:52 +0100

    P7-2

--- U7-PHP/P7/estructuras_control_rep.php
+++ U7-PHP/P7/estructuras_control_rep.php
@@ -11,6 +11,13 @@
             if($contador%2==0)
                 echo($contador . "<br>");
         }
+
+        $contador = 0;
+        do {
+            $contador++;
+            if($contador%2!=0)
+                echo($contador . "<br>");
+        } while($contador <= 50);
         ?>
     </body>
 </html>
