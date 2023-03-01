27828f055a79c1fb9adf0df3caf601a689678bec
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 17:35:13 +0100

    P6

--- U7-PHP/P6/estructuras_control.php
+++ U7-PHP/P6/estructuras_control.php
@@ -14,33 +14,33 @@
         const VELOCIDAD_MAX = 300;
 
         if($velocidad < VELOCIDAD_MAX && $aceleracion > ACELERACION_G)
-            echo("Todos los parámetros son correctos.");
+            echo("Todos los parámetros son correctos.<br>");
         else if($velocidad > VELOCIDAD_MAX)
-            echo("La velocidad es excesiva.");
+            echo("La velocidad es excesiva.<br>");
         else if($aceleracion < ACELERACION_G)
-            echo("La aceleración es insuficiente.");
+            echo("La aceleración es insuficiente.<br>");
 
         switch ($aceleracion) {
             case 0:
             case 1:
             case 2:
-                echo("Hay que aumentar la aceleración.");
+                echo("Hay que aumentar la aceleración.<br>");
                 break;
             case 3:
-                echo("Falta algo de aceleración.");
+                echo("Falta algo de aceleración.<br>");
                 break;
             case 4:
             case 5:
             case 6:
             case 7:
-                echo("La aceleración es perfecta.");
+                echo("La aceleración es perfecta.<br>");
                 break;
             case 8:
             case 9:
-                echo("Hay que decelerar por peligro de destrucción.");
+                echo("Hay que decelerar por peligro de destrucción.<br>");
                 break;
             default:
-                echo("La cifra no está contemplada.");
+                echo("La cifra no está contemplada.<br>");
                 break;
         }
         ?>
