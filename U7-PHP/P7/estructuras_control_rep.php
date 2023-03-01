141c0e772d29b570f2f0ce71c35e5c1e34992877
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 17:42:05 +0100

    P7-1

---
+++ U7-PHP/P7/estructuras_control_rep.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Asociación de matemáticas</title>
+    </head>
+
+    <body>
+        <?php
+        $contador = 0;
+        while($contador <= 50) {
+            $contador++;
+            if($contador%2==0)
+                echo $contador;
+        }
+        ?>
+    </body>
+</html>
