d1dfd55c5c0a29ef04e22a9dda6801a4e20ca750
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 18:30:43 +0100

    P8-1

---
+++ U7-PHP/P8/cadenas_caracteres.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Farmacia</title>
+    </head>
+
+    <body>
+        <?php
+        $bienvenida = "Bienvenido a la Farmacia de Eusebio Ramón";
+        $usuario = "Jose Antonio";
+        $saludo = "Buenas, Sr./Sra. $usuario";
+        $tarifa_reparto = "10€";
+        $precio_medicamento = "13€";
+
+        echo "$bienvenida <br>";
+        echo "¿Cuál es tu nombre? <br>";
+        echo "$saludo <br>";
+        echo "La tarifa de reparto del medicamento es de $tarifa_reparto. <br>";
+        echo "El precio del medicamento es de $precio_medicamento. <br>";
+        ?>
+    </body>
+</html>
