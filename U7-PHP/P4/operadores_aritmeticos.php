39ef1f06a756633c005b69528d0e134e7a2e6ae7
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 18:48:23 +0100

    Has to be const

--- U7-PHP/P4/operadores_aritmeticos.php
+++ U7-PHP/P4/operadores_aritmeticos.php
@@ -7,9 +7,9 @@
         <?php
         $precio_menu = 5.43;
         $comensales = 5;
-        const $descuento = 10/100; // 0.1
+        const descuento = 10/100; // 0.1
 
-        $descuento_menu = $precio_menu*$descuento;
+        $descuento_menu = $precio_menu*descuento;
         $total_menu = $precio_menu-$descuento_menu;
         $total = $total_menu*$comensales;
 
@@ -18,8 +18,8 @@
         echo('El total calculado para el número de comensales es de ' . $total . '€<br>');
 
         $ninios = 2;
-        const $descuento_ninios = 20/100; // 0.2
-        $total_menu = $precio_menu*$descuento_ninios;
+        const descuento_ninios = 20/100; // 0.2
+        $total_menu = $precio_menu*descuento_ninios;
         $total = $total_menu*$ninios;
 
         echo("El total calculado para el menú de los niños es de $total €.<br>");
