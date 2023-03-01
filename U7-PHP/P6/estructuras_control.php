9642adbfef3fcdd444e550f7cefff9d1d2f76027
PK2_Stimpy <thefernan1@gmail.com>
2023-03-01 17:24:50 +0100

    P6-1

---
+++ U7-PHP/P6/estructuras_control.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Agencia espacial</title>
+    </head>
+
+    <body>
+        <?php
+        $velocidad = 100;
+        $aceleracion = 40;
+        $altura = 105;
+
+        const GRAVITACIONAL = 6.674e11;
+        const ACELERACION_G = 9.8;
+        const VELOCIDAD_MAX = 300;
+
+        if($velocidad < VELOCIDAD_MAX && $aceleracion > ACELERACION_G)
+            echo("Todos los parámetros son correctos.");
+        else if($velocidad > VELOCIDAD_MAX)
+            echo("La velocidad es excesiva.");
+        else if($aceleracion < ACELERACION_G)
+            echo("La aceleración es insuficiente.");
+        ?>
+    </body>
+</html>
