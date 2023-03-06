ca7280f5814b31e5bb80804adecdbcc8a5d182d6
PK2_Stimpy <thefernan1@gmail.com>
2023-03-06 19:18:01 +0100

    Primos

---
+++ U7-PHP/P11-E/mcd.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Máximo común divisor</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    </head>
+
+    <body>
+        <?php
+
+        ?>
+    </body>
+</html>
---
+++ U7-PHP/P11-E/primos.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>Primos</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    </head>
+
+    <body>
+        <?php
+        const MAX_PRIMOS = 50;
+        $primos = array('2');
+        $num = $primos[count($primos) - 1];
+        $isp = true;
+        
+        while(count($primos) < 50) {
+            $arrCount = count($primos);
+            $num++;
+
+            for($i = 0,$isp=true; $i < $arrCount; $i++)
+                if($num % $primos[$i])
+                    $isp = false;
+            if($isp)
+                $primos[$arrCount] = $num;
+        }
+
+        echo "Num. Primos: <br>";
+        foreach($primos as $primo)
+            echo " - $primo<br>";
+        ?>
+    </body>
+</html>
