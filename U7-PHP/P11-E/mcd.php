3499c51abf82dd75a99c02c2170629243505e2f6
PK2_Stimpy <thefernan1@gmail.com>
2023-03-07 18:40:53 +0100

    mcd?

--- U7-PHP/P11-E/mcd.php
+++ U7-PHP/P11-E/mcd.php
@@ -8,7 +8,27 @@
         <?php
         const NUM_1 = 66;
         const NUM_2 = 980;
+        echo "NUM_1: " . NUM_1 . "; NUM_2: " . NUM_2 . "<br>";
         
+        $min = NUM_1, $max = NUM_2;
+        if(NUM_1 > NUM_2)
+            $min = NUM_2,
+            $max = NUM_1;
+        echo "MIN: $min; MAX: $max<br>";
+
+        $div = 1;
+        for($i = $min; $i > 1; $i--) {
+            $dmin = NUM_1 % $i;
+            $dmax = NUM_2 % $i;
+
+            if($dmin == 0 && $dmax == 0) {
+                $div = $i;
+                break;
+            }
+        }
+
+
+        echo "DIV: $div<br>";
         ?>
     </body>
 </html>
