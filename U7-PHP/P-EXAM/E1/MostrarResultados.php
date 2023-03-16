fd082824210b709b330d6cbca5ef6875578b20c9
PK2_Stimpy <thefernan1@gmail.com>
2023-03-16 16:17:19 +0100

    Added html simple form for numeros.html

---
+++ U7-PHP/P-EXAM/E1/MostrarResultados.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Ejercicio 1</title>
+        
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    </head>
+
+    <body>
+        
+    </body>
+</html>
